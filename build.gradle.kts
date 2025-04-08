d591365e670d3794c8afed8ed81e792e0d85dd46
martinsjavacode <8130295+martinsjavacode@users.noreply.github.com.>
2025-04-08 17:27:12 -0300

    test: Integrate JaCoCo for code coverage reporting and verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("idea")
     id("com.gradle.plugin-publish")
     id("org.jlleitschuh.gradle.ktlint")
+    jacoco
 }
 
 group = "io.github.martinsjavacode"
@@ -28,6 +29,7 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 gradlePlugin {
@@ -43,4 +45,47 @@ gradlePlugin {
             tags.set(listOf("avro", "generator", "serialization", "java", "avsc"))
         }
     }
+}
+
+jacoco {
+    toolVersion = "${property("jacocoVersion")}"
+    reportsDirectory = layout.buildDirectory.dir("reports/jacoco")
+}
+
+val excludeRulesDir =
+    fileTree(
+        layout.buildDirectory.dir("classes/kotlin/main")
+    ) {
+        exclude(
+            "**/extension/*",
+            "**/generated/*",
+        )
+    }
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("jacoco/test/html"))
+    }
+
+    classDirectories.setFrom(excludeRulesDir)
+}
+
+tasks.jacocoTestCoverageVerification {
+    classDirectories.setFrom(excludeRulesDir)
+
+    violationRules {
+        rule {
+            limit {
+                minimum = BigDecimal.valueOf(0.9)
+            }
+        }
+    }
+
+    dependsOn(tasks.jacocoTestReport)
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
