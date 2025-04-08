0fbbcf1f4f5f598c1c8b8710d8cdd155767d804f
martinsjavacode <8130295+martinsjavacode@users.noreply.github.com.>
2025-04-08 14:24:54 -0300

    feature: Enables support for decimal logical types in Avro schemas and null-safe annotations to generated classes.

--- src/main/kotlin/io/github/martinsjavacode/avro/extension/AvroPluginExtension.kt
+++ src/main/kotlin/io/github/martinsjavacode/avro/extension/AvroPluginExtension.kt
@@ -1,11 +1,11 @@
-package io.github.martinsjavacode.avro
+package io.github.martinsjavacode.avro.extension
 
 open class AvroPluginExtension {
-    var group: String? = null
-    var description: String? = null
     var sourceDir: String? = null
     var outputDir: String? = null
     var fieldVisibility: String = "PUBLIC"
     var stringType: String = "String"
     var optionalGetters: Boolean = false
+    var useDecimalLogical: Boolean = false
+    var createNullSafeAnnotations: Boolean = false
 }
